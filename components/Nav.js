727e317424b074283ff88934e416be68818e09e5
safayatalif <safayatalif2021@gmail.com>
2025-11-10 00:22:25 +0600

    nav design complated

--- components/Nav.js
+++ components/Nav.js
@@ -39,11 +39,22 @@ const Nav = () => {
   const router = useRouter();
   const pathname = router.pathname;
 
-  return <nav>
+  return <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed
+  h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen '>
     {/* inner */}
-    <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10  '>
-      {navData.map((item, index) => {
-        return <Link key={index} href={item.path} >{item.icon} </Link>
+    <div className='flex w-full xl:flex-col items-center justify-between
+     xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10
+      backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full '>
+      {navData.map((link, index) => {
+        return (
+        <Link
+        className={`${link.path === pathname && 'text-accent'}`}
+          key={index}
+          href={link.path} 
+          >
+          <div>{link.icon}</div>
+        </Link>
+        );
       })}
     </div>
   </nav>;
