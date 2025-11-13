6c87def043f7c2dc867eb40c598bacfc195dcb6f
safayatalif <safayatalif2021@gmail.com>
2025-11-14 00:02:29 +0600

    nav bar tooltip add

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,22 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import Transition from '../components/Transition';
+
+
+// router  
+
+import { useRouter } from 'next/router';
+
+// framer motion
+import { AnimatePresence, motion } from 'framer-motion';
+
+
+
+
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  // vedio duration = 28.27;
   return (
     <Layout>
       <Component {...pageProps} />
