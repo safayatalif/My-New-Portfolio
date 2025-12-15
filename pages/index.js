38f00e0d13c6e28ead0359e8dcb442b063535fcd
safayatalif <safayatalif2021@gmail.com>
2025-12-15 20:55:11 +0600

    home page avatar add

--- pages/index.js
+++ pages/index.js
@@ -19,23 +19,41 @@ const Home = () => {
     <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
       <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
         {/* title */}
-        <h1 className="h1">
+        <motion.h1 variants={fadeIn('down', 0.3)} initial="hidden" whileInView={"show"} className="h1">
           Transforming Ideas <br /> Into {' '}
           <span className="text-accent">Digital Reality</span>
-        </h1>
+        </motion.h1>
         {/* sub title */}
-        <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+        <motion.p variants={fadeIn('down', 0.4)} initial="hidden" whileInView={"show"} className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
           Excepteur reprehenderit Lorem id et voluptate dolore ad qui ad incididunt nulla excepteur veniam. Dolor nostrud reprehenderit aute nulla consecteturet Lorem cillum mollit quis enim exercitation sit nulla amet  tempor deserunt tempor eiusmod laborum.
-        </p>
+        </motion.p>
         {/* btn  */}
         <div className="flex justify-center xl:hidden relative">
           <ProjectsBtn></ProjectsBtn>
         </div>
+        <motion.div
+          variants={fadeIn('down', 0.6)}
+          initial="hidden"
+          whileInView={"show"}
+          className="hidden xl:flex"
+        >
+          <ProjectsBtn></ProjectsBtn>
+        </motion.div>
       </div>
     </div>
-    image
-    <div>
-      image
+    {/* image */}
+    <div className="w-[1200px] h-full absolute right-0 bottom-0">
+      {/* bg image  */}
+      <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0">
+        {/* particles */}
+        <div>particles</div>
+        {/* avatar image */}
+        <div className="">
+          <Avatar></Avatar>
+        </div>
+      </div>
+
+
     </div>
   </div>;
 };
